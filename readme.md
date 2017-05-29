ae46d6470a8b6e206c33837c1cec5cbf4b589fe5
Chris <capozzic1@gmail.com>
2017-05-29 21:46:05 +0900

    Add files

---
+++ readme.md
@@ -0,0 +1,1 @@
+This application gets the user's IP address, operating system info, and language.
---
+++ server.js
@@ -0,0 +1,30 @@
+
+let express = require('express');
+let app = express();
+let debug = require('debug')('log');
+let os = require('os');
+let accepts = require('accepts');
+let port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+res.sendFile(__dirname + "/index.html");
+
+let ostype = os.type();
+let osarch = os.arch();
+let lang = accepts(req).languages();
+let userInfo = { userIP: req.ip,
+                 userOS: {ostype,osarch},
+                 userLang: lang
+                }
+
+res.json(userInfo);
+debug(userInfo);
+
+
+
+//debug(req.connection.remoteAddress);
+});
+
+app.listen(port, () =>{
+  console.log('listening on ' + port);
+})
