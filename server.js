6311bb53ce8b861f1932d2bbf4fc738d3d68457b
Chris <capozzic1@gmail.com>
2017-05-30 12:57:10 +0900

    updated os

--- server.js
+++ server.js
@@ -4,17 +4,24 @@ let app = express();
 let debug = require('debug')('log');
 let os = require('os');
 let accepts = require('accepts');
+let useragent = require('useragent');
 let port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 res.sendFile(__dirname + "/index.html");
+//fix inaccuracies of osplatform and type, currently shows linux...
 
+let agent = useragent.parse(req.headers['user-agent']);
+//console.log(req.headers['user-agent']);
+let ip = req.connection.remoteAddress;
 let ostype = os.type();
 let osarch = os.arch();
 let osplatform = os.platform();
 let lang = accepts(req).languages();
-let userInfo = { userIP: req.ip,
-                 userOS: {osplatform,ostype,osarch},
+
+
+let userInfo = { userIP: ip,
+                 userOS: agent.os.family,
                  userLang: lang
                 }
 
@@ -23,7 +30,7 @@ debug(userInfo);
 
 
 
-//debug(req.connection.remoteAddress);
+
 });
 
 app.listen(port, () =>{
